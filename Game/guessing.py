cacd10b9df80cfc6f829488b7c65b43fabb61c6c
atanu-git-dubug <imatanu0@gmail.com>
2026-01-31 00:12:43 +0530

    Created Guessing Game and a InstaBot

---
+++ Game/guessing.py
@@ -0,0 +1,31 @@
+import random
+
+def guessing_game(user,user_name,computer_name):
+    computer = random.randint(1,10)
+    user = user
+
+    winner = user_name if user == computer else computer_name
+
+    return f"{user_name} chose: {user}, {computer_name} chose: {computer}.\nWinner is: {winner}"
+
+def get_name():
+
+    name = input("Enter your name: ")
+
+    computer_name = input("You want to set a name for computer? (yes/no): ")
+
+    c_name = input("Enter the computer's name: ") if computer_name.lower() == "yes" else "Computer"
+
+    return name, c_name
+
+def get_guess_from_user():
+
+    user = int(input("Enter a number between 1 and 10: "))
+    return user
+
+if __name__ == "__main__":
+
+    names = get_name()
+    user_input = get_guess_from_user()
+    result = guessing_game(user_input,names[0],names[1])
+    print(result)
---
+++ InstaBot/main.py
@@ -0,0 +1,43 @@
+# create a virtual environment and install instabot
+# python -m venv instabot_env (to create a virtual environment)
+# source instabot_env/bin/activate (to activate the virtual environment)
+# pip install instabot (to install instabot library)
+
+#importing the Bot class from instabot library
+from instabot import Bot
+
+# Create an instance of the Bot class
+bot = Bot()
+
+# Login to Instagram
+bot.login(
+    username="everwhat720",
+    password="atanuvk18"
+)
+
+# to follow a user
+bot.follow("username_to_follow")
+
+# to unfollow a user
+bot.unfollow("username_to_unfollow")
+
+# to like a post by its URL
+bot.like("https://www.instagram.com/p/POST_ID/")
+
+# to comment on a post by its URL
+bot.comment("https://www.instagram.com/p/POST_ID/", "Nice post!")
+
+# to send a direct message to a user
+bot.send_message("Hello! This is a test message.", ["recipient_username"])
+
+# to upload a photo with a caption
+bot.upload_photo("path/to/photo.jpg", caption="This is a caption for the photo.")
+
+# to unfollow all users who do not follow back
+bot.unfollow_non_followers()
+
+# to unfollow all users
+bot.unfollow_all()
+
+# to logout from Instagram
+bot.logout()
